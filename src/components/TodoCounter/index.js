3755cfb374561823472325d5636a0f9713e352b6
snatera <snatera.ve@gmail.com>
2024-02-12 12:51:35 -0300

    Setting packahe.json

--- src/components/TodoCounter/index.js
+++ src/components/TodoCounter/index.js
@@ -3,13 +3,13 @@ import { TodoContext } from 'components/TodoContext';
 import './TodoCounter.css';
 
 // Una forma de enviar estilos
-const estilos = {
+/*const estilos = {
   backgroundColor:'red',
   fontSize: '24px',
   textAlign:'center',
   margin: 0,
   padding:'48px'
-}
+}*/
 // props = se puede destructurar
 // <h1 style={estilos}>
 function TodoCounter(){
